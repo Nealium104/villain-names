32ab8a66a415688e7e4acc657f38dfa79cf507f3
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-02-16 20:27:35 -0500

    fixed typo

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.get('/api/:name', (request, response)=>{
 
 
 //Listen
-app.listen(prcoess.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Rad my dude!`)
 })
